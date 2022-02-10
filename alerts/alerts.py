734673c768bbea5f3c2c6f9768bfdbc4f721bd11
Arvid <arvid.b901@gmail.com>
2022-02-09 21:38:44 -0500

    Make alerts more generic, to accept any history type to check for change

--- alerts/alerts.py
+++ alerts/alerts.py
@@ -1,8 +1,8 @@
 from enum  import Enum, auto
-from datetime import datetime
+from bson.objectid import ObjectId
 from coin.coinpair import CoinPair, InvalidCoinPair
 from utils.redis_handler import redis
-from utils.db import db
+from utils.db import db, alerts_collection
 from datetime import datetime
 
 class AlertType(Enum):
@@ -42,27 +42,6 @@ class AlertRunnerMixin:
         if trigger_alert:
             self.generate_alert(msg)
 
-class AlertBase:
-    TYPE = None
-
-    def __init__(self, alert_id=None, coin_pair_id=None, threshold=None):
-        self.cache = redis()
-        self.db = db['alerts']
-        self.alert_id = alert_id
-
-        if alert_id:
-            alert_info = self.db.find_one({'_id':self.alert_id})
-            self.coin_pair_id = alert_info.get('coin_pair_id')
-            self.threshold = alert_info.get('threshold')
-        elif coin_pair_id is not None and threshold is not None:
-            print('Creating a new alert')
-            create_new = self.create_new(coin_pair_id,threshold)
-            if create_new:
-                self.__init__(alert_id=self.alert_id)
-            else:
-                raise AlertCreationError(self.TYPE.value, **{'alert_id': alert_id, 'coin': coin_pair_id, 'threshold': threshold})
-
-
     def check(self):
         NotImplementedError
 
@@ -85,27 +64,59 @@ class AlertBase:
             self.cache.set(cache_key, '1')
             return False
 
-    def create_new(self,coin_pair,threshold):
-        self.threshold = threshold
-        self.coin_pair_id = coin_pair
+class AlertBase:
+    TYPE = None
+    SUPPORTED_TRACKER_TYPES = ('price','volume')
+
+    def __init__(self, alert_id=None, coin_pair_id=None, tracker_type=None, threshold=None ):
+        self.cache = redis()
+        self.db = alerts_collection
+        self.alert_id = alert_id
+        create_new = all([coin_pair_id, tracker_type, threshold])
+
+        if alert_id:
+            alert_info = self.db.find_one({'_id':ObjectId(self.alert_id)})
+            self.coinpair = CoinPair(alert_info.get('coin_pair_id'))
+            self.threshold = alert_info.get('threshold')
+            self.tracker_type = alert_info.get('threshold')
+        elif create_new:
+            print('Creating a new alert')
+            create_new = self.create_new(coin_pair_id,threshold,tracker_type)
+            if create_new:
+                # When the alert is created self.alert_id is assigned to from the
+                # new object id when saving to mongo'
+                self.__init__(alert_id=self.alert_id)
+            else:
+                raise AlertCreationError(self.TYPE.value, **{'alert_id': alert_id, 'coin': coin_pair_id,
+                                                             'threshold': threshold,'tracker_type':tracker_type})
+
+    def create_new(self, coin_pair_id, threshold, tracker_type):
+        if tracker_type not in self.SUPPORTED_TRACKER_TYPES:
+            print(f'tracker type {tracker_type} is not supported.')
+            return
         try:
-            CoinPair(coin_pair)
+            self.coinpair = CoinPair(coin_pair_id)
         except InvalidCoinPair:
-            print(f'Invalid coin pair {coin_pair},skipping creation')
+            print(f'Invalid coin pair {coin_pair_id},skipping creation')
             return
 
         alert_id = self.db.insert_one({})
         self.alert_id = alert_id.inserted_id
+        self.threshold = threshold
+        self.tracker_type = tracker_type
         self.save()
 
         return True
+
+
+    #Todo: perhaps track if alert expires
 
     def save(self):
         if not self.alert_id:
             print("Can't save alert without id assigned")
             return
         query = {'$set': {'alert_type':self.TYPE.value,'threshold':self.threshold,
-                          'coin_pair_id':self.coin_pair_id, 'insert_time':datetime.utcnow()}}
+                          'coin_pair_id':self.coinpair.pair_id, 'insert_time':datetime.utcnow()}}
         self.db.find_one_and_update({'_id':self.alert_id},query)
 
 
@@ -114,12 +125,16 @@ class PercentChangeAlert(AlertBase, AlertRunnerMixin):
 
     def check(self):
         # get the most recent price history
-        price_history = self.coin.price_history
-        current_price = price_history[0]
-        current_price_val,current_price_insert_time = current_price.price, current_price.insert_time
+        history_type = self.tracker_type
+        if history_type == 'price':
+            history_values =  self.coinpair.pair_history()
+        elif history_type == 'volume':
+            pass
+        current_type_value = history_values[0]
+        current_price_val,current_price_insert_time = current_type_value.price, current_type_value.insert_time
 
         #Start this the one after the price we are checking
-        for price in price_history[1:]:
+        for value in history_values[1:]:
             value = price.price
             trigger_alert, msg, change = self._percent_change(current_price_val, value)
             if trigger_alert and not self.already_alerted(current_price, price):
@@ -165,7 +180,6 @@ class AlertRunner(AlertRunnerMixin):
         self.db = db['alerts']
         self.alerts = self.get_alerts()
 
-
     def get_alerts(self):
         all_alerts = []
         alerts = self.db.find({},{'alert_type':1,'_id':1})
@@ -181,6 +195,7 @@ class AlertRunner(AlertRunnerMixin):
                 alert.run_check()
             except NoCoinForAlert:
                 print('skipping check run for alert. no coin assigned')
+
 
 class WatchlistAlert(AlertBase):
     def __init__(self, watchlist_id,coin,threshold,alert_type):
@@ -204,6 +219,6 @@ class WatchlistAlert(AlertBase):
 # WatchlistAlert('1bcb9699-781c-11ec-a3b5-1c1b0deb7f19','ADA-USD',1,'percent').save()
 
 
-PercentChangeAlert(coin_pair_id='61f5814d32e2534f6e8e0ef7',threshold=9)
+PercentChangeAlert(coin_pair_id='61f5814d32e2534f6e8e0ef7',threshold=9,tracker_type='prdice')
 # ADA-USD
 # 61f5814d32e2534f6e8e0ef7
