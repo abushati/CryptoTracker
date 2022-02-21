3a4bf512af87e0b51fd6239492575adadad7ec1d
Arvid <arvid.b901@gmail.com>
2022-02-21 14:11:09 -0500

    include clean db util.

--- utils/db.py
+++ utils/db.py
@@ -9,3 +9,11 @@ coin_history_collection = db['coin_history']
 alerts_collection = db['alerts']
 
 desc_sort = pymongo.DESCENDING
+
+def clean_collection(collection_name):
+    if collection_name == 'alerts':
+        col = alerts_collection
+    elif collection_name == 'coin_history':
+        col = coin_history_collection
+
+    col.delete_many({})
