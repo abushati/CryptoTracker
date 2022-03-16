8b56eecf9d3651e087b89fd3e23abca3d4f68d6a
Arvid Bushati <arvid.b901@gmail.com>
2022-03-15 22:48:54 -0400

    working on coinpair page

--- frontend/cryptotracker/pages/coinpair/[coinpair_id].js
+++ frontend/cryptotracker/pages/coinpair/[coinpair_id].js
@@ -1,4 +1,15 @@
 function CoinPair (coinpair_id) {
+    const [coinpairData, setData] = useState([])
+    const [isLoading, setLoading] = useState(false)
+    useEffect(() => {
+        fetch('http://localhost:5000/coinpair/'+prop)
+            .then((res) => res.json())
+            .then((data) => {
+                let coinpairs = data.coinpairs
+                setData(coinpairs)
+                setLoading(false)
+            })
+    }, [])
     return (
         <div>
             Hi from coin pair page
