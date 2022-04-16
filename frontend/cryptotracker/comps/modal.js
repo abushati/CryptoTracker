b151a6846130b5f05a5abd56cf237e793c7d3cbd
Arvid <arvid.b901@gmail.com>
2022-04-16 00:56:22 -0400

    implemented percent change/refactored formfields/added save button logic

--- frontend/cryptotracker/comps/modal.js
+++ frontend/cryptotracker/comps/modal.js
@@ -11,103 +11,138 @@ import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormLabel from '@mui/material/FormLabel';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 //https://devrecipes.net/modal-component-with-next-js/
-let setNewAlertData = {}
+// let setNewAlertData = {}
 const Modal = ({ show, onClose, coinInfo }) => {
     const [isBrowser, setIsBrowser] = useState(false);
     const [alertType, setAlertType] = useState("");   
     const [formFields, setFormFields] = useState ([])
-    // const [newAlertData, setNewAlertData] = useState({})
+    const [newAlertData, setNewAlertData] = useState({})
+    const [enableSaveButton, setEnableSaveButton] = useState(false)
+    const [successSave, setSuccessSave] = useState(false)
 
-    
+    const Alert = React.forwardRef(function Alert(props, ref) {
+      return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+    });
+
     useEffect(() => {
       setIsBrowser(true);
     }, []);
   
     const handleCloseClick = (e) => {
       e.preventDefault();
+      setNewAlertData({})
+      setFormFields([])
+      setAlertType("")
+      setEnableSaveButton(false)
+      setSuccessSave(false)
       onClose();
 
     };
 
-
   const rebuildForm = (alertType) => {
       let type = alertType
-      if (type != 'price') {
-        setFormFields([])
-        return
-      }
-  //   'label':<InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>,
-                              //   'input': <OutlinedInput
-                              //               id="outlined-adornment-amount"
-                              //               value={0}
-                              //               onChange={(e) => setNewAlertData['threshold']=e.target.value}
-                              //               startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                              //               label="Amount"
-                              //             />
-                              // },
-      console.log('here')
-      let formBodyFields = {'price': 
-                              {'priceValue':{
-                                'label':<InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>,
-                                'input': <OutlinedInput
+      let additionalFormFields = {'price': 
+                                      <div>
+                                        <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+                                          <OutlinedInput
+                                              id="outlined-adornment-amount"
+                                              value={newAlertData['threshold']}
+                                              onChange={(e) => addDataToAlert('threshold',e.target.value)}
+                                              startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                                              label="Amount"
+                                            />
+                                        <FormLabel id="demo-radio-buttons-group-label">Condition</FormLabel>
+                                          <RadioGroup
+                                              aria-labelledby="demo-controlled-radio-buttons-group"
+                                              name="controlled-radio-buttons-group"
+                                              value={newAlertData['threshold_condition']}
+                                              onChange={(e) => addDataToAlert('threshold_condition',e.target.value)}
+                                            >
+                                              <FormControlLabel value="above" control={<Radio />} label="Above Price" />
+                                              <FormControlLabel value="below" control={<Radio />} label="Below Price" />
+                                            </RadioGroup>
+                                      </div> ,
+                                'percent':
+                                    <div>
+                                      <InputLabel htmlFor="outlined-adornment-amount">Percent Change</InputLabel>
+                                        <OutlinedInput
                                             id="outlined-adornment-amount"
-                                            value={setNewAlertData['threshold']}
-                                            onChange={(e) => setNewAlertData['threshold']=e.target.value}
-                                            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                                            value={newAlertData['threshold']}
+                                            onChange={(e) => addDataToAlert('threshold',e.target.value)}
+                                            startAdornment={<InputAdornment position="start">%</InputAdornment>}
                                             label="Amount"
                                           />
-                              },
-                              'priceCondition': {
-                                'label':<FormLabel id="demo-radio-buttons-group-label">Condition</FormLabel>,
-                                'input':  <RadioGroup
+                                      <FormLabel id="demo-radio-buttons-group-label">Condition</FormLabel>
+                                        <RadioGroup
                                             aria-labelledby="demo-controlled-radio-buttons-group"
                                             name="controlled-radio-buttons-group"
-                                            value={setNewAlertData['threshold_condition']}
-                                            onChange={(e) => {setNewAlertData['threshold_condition']=e.target.value}}
+                                            value={newAlertData['threshold_condition']}
+                                            onChange={(e) => addDataToAlert('threshold_condition',e.target.value)}
                                           >
-                                            <FormControlLabel value="above" control={<Radio />} label="Above Price" />
-                                            <FormControlLabel value="below" control={<Radio />} label="Below Price" />
+                                            <FormControlLabel value="increase" control={<Radio />} label="Percent Increase" />
+                                            <FormControlLabel value="decrease" control={<Radio />} label="Percent Decrease" />
                                           </RadioGroup>
-
+                                    </div>
                               }
-                            }}
-    
-    let formBody = formBodyFields[type]
-    let html = []
-    Object.keys(formBody).map((field) => {       
-      html.push(formBody[field]['label'])
-      html.push(formBody[field]['input'])
-    })
-    console.log(html)
-    setFormFields(html)
+
+    let formBody = additionalFormFields[type]
+    setFormFields(formBody)
+  }
+
+
+  useEffect(() => {
+    let requiredFields = ['alert_type', 'threshold', 'threshold_condition']
+    let alertFields = Object.keys(newAlertData)
+    console.log(alertFields)
+    const enableSave = requiredFields.every(e => alertFields.includes(e))
+    if (enableSave) setEnableSaveButton(true)
+  }, [newAlertData]);
+
+  const addDataToAlert = (test, value) => {
+    const updatedValue = {}
+    updatedValue[test]=value
+    setNewAlertData(newAlertData => ({
+      ...newAlertData,
+      ...updatedValue
+    }));
   }
 
   const saveAlert = (e) => {
     e.preventDefault();
-    setNewAlertData['coin_sym']=coinInfo.coinpair_sym
-    console.log(setNewAlertData)
-    alert('saving alert')
+    addDataToAlert('coin_sym',coinInfo.coinpair_sym)
+    console.log(newAlertData)
+
     fetch('http://localhost:5000/alerts',{
       mode: 'no-cors',
       method: 'POST', // or 'PUT'
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(setNewAlertData),
+      body: JSON.stringify(newAlertData),
     })
+    //Todo: to check if the call was actually successful
+    setSuccessSave(true)
   }
 
   const handleAlertChange = (e) =>{
     const value = e.target.value
-    setNewAlertData={}
-    setNewAlertData['alert_type']=value
+    setNewAlertData({})
+    addDataToAlert('alert_type',value)
     rebuildForm(value)
     setAlertType(value)
   }
 
     const modalContent = show ? (
       <StyledModalOverlay>
+        <Snackbar open={successSave} autoHideDuration={6000} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}  onClose={() => {setSuccessSave(false)}}>
+              <Alert severity="success" sx={{ width: '100%' }}>
+                Alert Saved!
+              </Alert>
+            </Snackbar>
         <StyledModal>
           <StyledModalHeader>
             <a href="#" onClick={handleCloseClick}>x</a>
@@ -135,9 +170,9 @@ const Modal = ({ show, onClose, coinInfo }) => {
                 </Select>
                 </FormControl>
                 <div>
-                  {formFields.map(field => field)}
+                  {formFields}
                 </div>
-                <button type="submit">Save Alert</button>
+                <Button variant="contained" color="success" type="submit" disabled={!enableSaveButton}>Save Alert</Button>
               </form>
             </div>
             
