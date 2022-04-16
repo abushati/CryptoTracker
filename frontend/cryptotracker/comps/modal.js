1f9ce91d01c25cc3979949e356eb6b71ba07530b
Arvid <arvid.b901@gmail.com>
2022-04-16 15:40:05 -0400

    Finished up alert modal

--- frontend/cryptotracker/comps/modal.js
+++ frontend/cryptotracker/comps/modal.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 import styled from "styled-components";
 import MenuItem from '@mui/material/MenuItem';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import OutlinedInput from '@mui/material/OutlinedInput';
@@ -14,6 +14,7 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Button from '@mui/material/Button';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
+
 //https://devrecipes.net/modal-component-with-next-js/
 // let setNewAlertData = {}
 const Modal = ({ show, onClose, coinInfo }) => {
@@ -22,7 +23,8 @@ const Modal = ({ show, onClose, coinInfo }) => {
     const [formFields, setFormFields] = useState ([])
     const [newAlertData, setNewAlertData] = useState({})
     const [enableSaveButton, setEnableSaveButton] = useState(false)
-    const [successSave, setSuccessSave] = useState(false)
+    const [showSnackBar, setShowSnackBar] = useState(false)
+    const [snackBarInfo, setSnackBarInfo] = useState({})
 
     const Alert = React.forwardRef(function Alert(props, ref) {
       return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
@@ -38,15 +40,21 @@ const Modal = ({ show, onClose, coinInfo }) => {
       setFormFields([])
       setAlertType("")
       setEnableSaveButton(false)
-      setSuccessSave(false)
+      setShowSnackBar(false)
       onClose();
 
     };
 
+  useEffect(()=>{
+    if (isBrowser) {
+      console.log('here')
+      addDataToAlert('coin_sym',coinInfo.coinpair_sym)}
+  },[isBrowser])
+
   const rebuildForm = (alertType) => {
       let type = alertType
       let additionalFormFields = {'price': 
-                                      <div>
+                                    <AdditionalModelFields>
                                         <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
                                           <OutlinedInput
                                               id="outlined-adornment-amount"
@@ -65,9 +73,10 @@ const Modal = ({ show, onClose, coinInfo }) => {
                                               <FormControlLabel value="above" control={<Radio />} label="Above Price" />
                                               <FormControlLabel value="below" control={<Radio />} label="Below Price" />
                                             </RadioGroup>
-                                      </div> ,
+                                      </AdditionalModelFields>,
+                                     
                                 'percent':
-                                    <div>
+                                  <AdditionalModelFields>
                                       <InputLabel htmlFor="outlined-adornment-amount">Percent Change</InputLabel>
                                         <OutlinedInput
                                             id="outlined-adornment-amount"
@@ -86,7 +95,7 @@ const Modal = ({ show, onClose, coinInfo }) => {
                                             <FormControlLabel value="increase" control={<Radio />} label="Percent Increase" />
                                             <FormControlLabel value="decrease" control={<Radio />} label="Percent Decrease" />
                                           </RadioGroup>
-                                    </div>
+                                    </AdditionalModelFields>
                               }
 
     let formBody = additionalFormFields[type]
@@ -97,7 +106,6 @@ const Modal = ({ show, onClose, coinInfo }) => {
   useEffect(() => {
     let requiredFields = ['alert_type', 'threshold', 'threshold_condition']
     let alertFields = Object.keys(newAlertData)
-    console.log(alertFields)
     const enableSave = requiredFields.every(e => alertFields.includes(e))
     if (enableSave) setEnableSaveButton(true)
   }, [newAlertData]);
@@ -111,21 +119,43 @@ const Modal = ({ show, onClose, coinInfo }) => {
     }));
   }
 
+  const showSnack = (success)=>{
+    if (success){
+      var msg = 'Alert Saved!'
+      var type = 'success'
+    }
+    else {
+      var msg = 'Failed To Save Alert!'
+      var type = 'error'
+    }
+
+    const updatedValue = {}
+    updatedValue['msg']=msg
+    updatedValue['type']=type
+    setSnackBarInfo(snackBarInfo=>({...snackBarInfo,...updatedValue}))
+    setShowSnackBar(true)
+  }
+
   const saveAlert = (e) => {
     e.preventDefault();
-    addDataToAlert('coin_sym',coinInfo.coinpair_sym)
-    console.log(newAlertData)
-
+    let data = newAlertData
+    data['coin_sym']=coinInfo.coinpair_sym
     fetch('http://localhost:5000/alerts',{
-      mode: 'no-cors',
+      // mode: 'no-cors',
       method: 'POST', // or 'PUT'
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(newAlertData),
+    }).then(res =>{
+      if (res.status == 200){
+        showSnack(true)
+      }
+      else{
+        showSnack(false)
+      }
+      console.log(res.status)
     })
-    //Todo: to check if the call was actually successful
-    setSuccessSave(true)
   }
 
   const handleAlertChange = (e) =>{
@@ -136,43 +166,45 @@ const Modal = ({ show, onClose, coinInfo }) => {
     setAlertType(value)
   }
 
+
+  
+
     const modalContent = show ? (
       <StyledModalOverlay>
-        <Snackbar open={successSave} autoHideDuration={6000} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}  onClose={() => {setSuccessSave(false)}}>
-              <Alert severity="success" sx={{ width: '100%' }}>
-                Alert Saved!
+        <Snackbar open={showSnackBar} autoHideDuration={6000} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}  onClose={() => {setShowSnackBar(false)}}>
+              <Alert severity={snackBarInfo.type} sx={{ width: '100%' }}>
+                  {snackBarInfo.msg}
               </Alert>
             </Snackbar>
         <StyledModal>
           <StyledModalHeader>
+            <div style={{width:"85%"}}>Alert Creation</div>
             <a href="#" onClick={handleCloseClick}>x</a>
           </StyledModalHeader>
-          <div>Alert Creation</div>
-          <StyledModalBody>From Alert creation modal
-            <div>
-              SYM:{coinInfo.coinpair_sym}
-              Current Price:{coinInfo.price_value}
-              Price Time:{coinInfo.price_update}
+          <StyledModalBody>
+            <div style={{fontSize:"1.2em",padding:'10px 0px'}}>
+              <div>SYM:{coinInfo.coinpair_sym}</div>
+              <div>Current Price:{coinInfo.price_value}</div>
+              <div>Price Time:{coinInfo.price_update}</div>
             </div>
             <div>
-              <label for="coinSym">SYM:{coinInfo.coinpair_sym}</label>
               <form onSubmit={saveAlert} method="post">
-              <FormControl sx={{ m: 1, minWidth: 120 }} required>           
-                <InputLabel id="select-alert-label">Alert Type</InputLabel>
-                <Select
-                  labelId="select-alert-label"
-                  id="alert-select"
-                  label={alertType}
-                  onChange={handleAlertChange}
-                >
-                  <MenuItem value="price">Price Alert</MenuItem>
-                  <MenuItem value="percent">Percent Change Alert</MenuItem>
-                </Select>
-                </FormControl>
-                <div>
-                  {formFields}
-                </div>
-                <Button variant="contained" color="success" type="submit" disabled={!enableSaveButton}>Save Alert</Button>
+                <FormControl sx={{ m: 1, minWidth: 120 }} required>           
+                  <InputLabel id="select-alert-label">Alert Type</InputLabel>
+                  <Select
+                    labelId="select-alert-label"
+                    id="alert-select"
+                    label={alertType}
+                    onChange={handleAlertChange}
+                  >
+                    <MenuItem value="price">Price Alert</MenuItem>
+                    <MenuItem value="percent">Percent Change Alert</MenuItem>
+                  </Select>
+                  </FormControl>
+                  <div>
+                    {formFields}
+                  </div>
+                  <Button variant="contained" color="success" type="submit" disabled={!enableSaveButton}>Save Alert</Button>
               </form>
             </div>
             
@@ -192,13 +224,16 @@ const Modal = ({ show, onClose, coinInfo }) => {
   };
   
   const StyledModalBody = styled.div`
-    padding-top: 10px;
+      padding:'10px'
   `;
   
   const StyledModalHeader = styled.div`
     display: flex;
-    justify-content: flex-end;
     font-size: 25px;
+    border-bottom: gray;
+    border-width: thin;
+    border-bottom-style: solid;
+    padding-bottom: 10px;
   `;
   
   const StyledModal = styled.div`
@@ -220,4 +255,9 @@ const Modal = ({ show, onClose, coinInfo }) => {
     background-color: rgba(0, 0, 0, 0.5);
   `;
   
+  const AdditionalModelFields = styled.div`
+  display: flex;
+  flex-direction:column;
+`;
+
   export default Modal;
