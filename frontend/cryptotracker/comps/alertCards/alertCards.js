642aed2d0958c394afc8914cfe64746dba4fc050
arvid <arvid.b901@gmail.com>
2022-04-27 19:05:32 -0400

    showing valid actions per alert type

--- frontend/cryptotracker/comps/alertCards/alertCards.js
+++ frontend/cryptotracker/comps/alertCards/alertCards.js
@@ -1,17 +1,42 @@
 import styles from './alertCard.module.css'
+const {useState} = require("react");
 
 export const AlertCardType = Object.freeze({
   INFO : 'INFO',
   GENERATION : 'GENERATION'
 })
 
+export const CardActions = {
+  DELETE : 'DELETE',
+  MARK_READ : 'MARK_READ'
+}
 
 function AlertCard (props){
+    const [showActions, setShowActions] = useState(false)
     let cardHeader = props.cardHeader
     let cardBody = props.cardBody
     let type = props.type
+    
+    
+    let cardActions = {
+      'INFO': [CardActions.DELETE],
+      'GENERATION':[CardActions.MARK_READ]
+    }
 
-    const getClassname = () => {
+    const handleCardClick = () =>{
+      if (showActions){
+        setShowActions(false)
+        return 
+      }
+
+      let actions = cardActions[type]
+      console.log(actions)
+      for (const action in actions){
+        break
+      }
+    }
+
+    const getClassname = () => {  
       if (type == AlertCardType.GENERATION){
         return styles.generatedAlert
       }
@@ -21,12 +46,17 @@ function AlertCard (props){
     }
 
     return (      
-        <div className={getClassname()}>
+        <div className={getClassname()} onClick={handleCardClick}>
+          <div>
           <div>
             {cardHeader}
           </div>
           <div>
             {cardBody}
+          </div>
+          </div>
+          <div className={styles.action}>
+            test
           </div>
         </div>
   )
