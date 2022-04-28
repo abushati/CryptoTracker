f9d629ceb8e97730ed2df0e0af5dcd1c14508c6e
arvid <arvid.b901@gmail.com>
2022-04-28 09:37:59 -0400

    Action buttons positioning and displaying

--- frontend/cryptotracker/comps/alertCards/alertCards.js
+++ frontend/cryptotracker/comps/alertCards/alertCards.js
@@ -1,4 +1,6 @@
 import styles from './alertCard.module.css'
+// import '@fortawesome/fontawesome-free/css/all.css';
+
 const {useState} = require("react");
 
 export const AlertCardType = Object.freeze({
@@ -8,7 +10,13 @@ export const AlertCardType = Object.freeze({
 
 export const CardActions = {
   DELETE : 'DELETE',
-  MARK_READ : 'MARK_READ'
+  MARK_READ : 'MARK_READ',
+  EDIT : 'EDIT'
+}
+
+let cardActions = {
+  'INFO': [CardActions.DELETE, CardActions.EDIT],
+  'GENERATION':[CardActions.MARK_READ]
 }
 
 function AlertCard (props){
@@ -16,24 +24,15 @@ function AlertCard (props){
     let cardHeader = props.cardHeader
     let cardBody = props.cardBody
     let type = props.type
+    let actions = cardActions[type]
     
-    
-    let cardActions = {
-      'INFO': [CardActions.DELETE],
-      'GENERATION':[CardActions.MARK_READ]
-    }
 
     const handleCardClick = () =>{
       if (showActions){
         setShowActions(false)
         return 
       }
-
-      let actions = cardActions[type]
-      console.log(actions)
-      for (const action in actions){
-        break
-      }
+      setShowActions(true)
     }
 
     const getClassname = () => {  
@@ -43,6 +42,13 @@ function AlertCard (props){
       else if (type == AlertCardType.INFO){
         return styles.alert
       } 
+    }
+
+    const getActionButton = (action) => {
+      let a = <div className={styles.action}>
+          x
+      </div>
+      return a
     }
 
     return (      
@@ -55,9 +61,14 @@ function AlertCard (props){
             {cardBody}
           </div>
           </div>
-          <div className={styles.action}>
-            test
-          </div>
+          {showActions ? 
+            <div className={styles.actions}>
+              {
+                actions.map(action => getActionButton(action))
+              }
+            </div>
+            : ''
+          }
         </div>
   )
 }
