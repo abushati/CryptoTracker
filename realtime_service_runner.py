1da2f51b47f9eecea30d5d694c3c4e3e9eccb1e2
arvid <arvid.b901@gmail.com>
2022-05-04 09:31:40 -0400

    created by realtime service init

---
+++ realtime_service_runner.py
@@ -0,0 +1,8 @@
+from realtime_updater.realtime_service_init import run
+import argparse
+
+parser = argparse.ArgumentParser(description='Start a realtime service')
+parser.add_argument('-s', '--service', help="Service name [webhook,updater]", required=True)
+args = parser.parse_args()
+print(args.service)
+run(args.service)
